6af9138d9bf3838c0803a7db59c34cc6a2769210
Renat Abaidulin <ares566@ya.ru>
2022-02-12 13:51:16 +0300

    thread::spawn(reading_from_mpu)

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,26 @@ use embedded_hal::blocking::delay::DelayMs;
 use i2cdev::linux::LinuxI2CError;
 use linux_embedded_hal::{Delay, I2cdev};
 use mpu6050::{device::MOT_DETECT_STATUS, *};
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 use troyka_hat::*;
 
 //     let mut log = new_file_log("firmware.log");
 //     log.add_log("Start");
 
+#[derive(Copy, Clone)]
 struct RPData {
     pitch: f32,
     roll: f32,
     yaw: f32,
 }
+
 const MPU6050_KOEF_COMPL: f32 = 0.9934;
 const DELTA_TIME: u16 = 250;
 fn main() -> Result<(), Mpu6050Error<LinuxI2CError>> {
+    let (tx, rx) = mpsc::channel();
+
     let mut delay = Delay;
 
     let i2c = I2cdev::new("/dev/i2c-1").map_err(Mpu6050Error::I2c)?;
@@ -31,11 +38,38 @@ fn main() -> Result<(), Mpu6050Error<LinuxI2CError>> {
     let mut th = troyka_hat::TroykaHat::new(i2c);
 
     mpu.init(&mut delay)?;
+    thread::spawn(|| reading_from_mpu(mpu, tx));
 
     th.init(&mut delay);
     th.pwm_freq(50);
 
+    for rp_data in rx {
+        
+        let mid_serv_val: f32 = (28.0 + 8.0) / 2.0;
+        let mut serv_val: u8 = (mid_serv_val + rp_data.roll / 6.9) as u8;
+
+        if rp_data.roll > 45.0 {
+            serv_val = 28
+        }
+        if rp_data.roll < -45.0 {
+            serv_val = 8
+        }
+
+        th.analog_write(7, serv_val);
+
+        println!(
+            "Angles: pitch={0} ,roll={1}; Servo: {2}",
+            rp_data.pitch, rp_data.roll, serv_val
+        );
+        println!("_____________________________________");
+    }
+
+    Ok(())
+}
+
+fn reading_from_mpu(mut mpu: Mpu6050<I2cdev>, tx: mpsc::Sender<RPData>) {
     let mut count: u16 = 0;
+
     let mut rp_data = RPData {
         pitch: 0.0,
         roll: 0.0,
@@ -45,12 +79,12 @@ fn main() -> Result<(), Mpu6050Error<LinuxI2CError>> {
         // TODO MPU initial calibration
 
         // get roll and pitch estimate
-        let acc = mpu.get_acc_angles()?;
+        let acc = mpu.get_acc_angles().unwrap();
         let roll4macc: f32 = acc.data[0] / PI_180;
         let pitch4macc: f32 = acc.data[1] / PI_180;
 
         // get gyro data, scaled with sensitivity
-        let gyro = mpu.get_gyro()?;
+        let gyro = mpu.get_gyro().unwrap();
 
         rp_data.pitch += (gyro.data[1] / PI_180) * (DELTA_TIME / 1000) as f32;
         rp_data.roll += (gyro.data[0] / PI_180) * (DELTA_TIME / 1000) as f32;
@@ -70,31 +104,13 @@ fn main() -> Result<(), Mpu6050Error<LinuxI2CError>> {
             rp_data.pitch * (1.0 - MPU6050_KOEF_COMPL) + pitch4macc * MPU6050_KOEF_COMPL;
         rp_data.roll = rp_data.roll * (1.0 - MPU6050_KOEF_COMPL) + roll4macc * MPU6050_KOEF_COMPL;
 
-        // servo testing
-        let mid_serv_val: f32 = (28.0 + 8.0) / 2.0;
-        let mut serv_val: u8 = (mid_serv_val + rp_data.roll / 6.9) as u8;
+        tx.send(rp_data).unwrap();
+        thread::sleep(Duration::from_millis(DELTA_TIME as u64));
 
-        if rp_data.roll > 45.0 {
-            serv_val = 28
-        }
-        if rp_data.roll < -45.0 {
-            serv_val = 8
-        }
-
-        th.analog_write(7, serv_val);
-
-        println!(
-            "Углы: pitch={0} ,roll={1}, serv={2}",
-            rp_data.pitch, rp_data.roll, serv_val
-        );
-        println!("_____________________________________");
-
-        delay.delay_ms(DELTA_TIME);
         count += 1;
         if count > 500 {
-            mpu.reset_device(&mut delay)?;
+            //mpu.reset_device(&mut delay)?;
             break;
         }
     }
-    Ok(())
 }
